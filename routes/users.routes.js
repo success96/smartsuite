469c05b3291e060d94b95db92ff2446072edb962
success96 <lfawas6@gmail.com>
2024-07-11 19:28:21 +0100

    Bug fix

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { createUser, userLogin, getUserById, getUserOrgById, getUserOrg } = require('../controllers/users.controller.js')
 
-router.post('/auth/register', createUser).get('/auth/login', userLogin)
+router.post('/auth/register', createUser).post('/auth/login', userLogin)
 router.get('/api/users/:id', getUserById).get('/api/organisations', getUserOrg).get('/api/organisation/:orgId', getUserOrgById)
 
 
