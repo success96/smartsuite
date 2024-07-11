9c507806a3dd862f6a2e93a0e327b847e063977b
success96 <lfawas6@gmail.com>
2024-07-11 20:01:33 +0100

    additional detail added

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { createUser, userLogin, getUserById, getUserOrgById, getUserOrg } = require('../controllers/users.controller.js')
 
 router.post('/auth/register', createUser).post('/auth/login', userLogin)
-router.get('/api/users/:id', getUserById).get('/api/organisations', getUserOrg).get('/api/organisation/:orgId', getUserOrgById)
+router.get('/api/users/:id', getUserById).get('/api/organisations', getUserOrg).get('/api/organisations/:orgId', getUserOrgById)
 
 
 
